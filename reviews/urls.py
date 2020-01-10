f4248193800e10aeddda1128be8720d3eed02f89
oliviamcreativelabs <contactus@oliviamcreativelabs.com>
2020-01-10 16:03:19 -0600

    Added forms

--- reviews/urls.py
+++ reviews/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.review_list, name='review_list'),
     path('reviews/<review_id>', views.review_detail, name='review_detail'),
     path('whiskey', views.whiskey_list, name='whiskey_list'),
-    path('whiskey/', views.whiskey_detail, name='whiskey_detail')
+    path('whiskey/', views.whiskey_detail, name='whiskey_detail'),
+    path('whiskey/<whiskey_id>/add_review', views.add_review, name='add_review'),
 ]
+
--- reviews/views.py
+++ reviews/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import get_object_or_404, render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Review, Whiskey
+from .forms import ReviewForm
+import datetime
 from .models import Review, Whiskey
 
 '''
 We have defined four different views (one for each of the four different url mappings). 
 Each function gets at least a request object parameter, and optionally more parameters as specified in the url mapping. For example, the review_detail function gets also a review_id parameter as we specified in the mapping.
 '''
+#  =====================================================
+# gets a list of the latest 9 reviews and renders it using `reviews/list.html'.
 
 
 def review_list(request):
@@ -15,10 +21,16 @@ def review_list(request):
     return render(request, 'reviews/review_list.html', context)
 
 
+#  =====================================================
+# gets a review given its ID and renders it using review_detail.html.
+
 def review_detail(request, review_id):
     review = get_object_or_404(Review, pk=review_id)
     return render(request, 'reviews/review_detail.html', {'review': review})
 
+
+#  =====================================================
+# gets all the whiskey sorted by name and passes it to Whiskey_list.html to be rendered.
 
 def whiskey_list(request):
     # ℹ
@@ -27,6 +39,36 @@ def whiskey_list(request):
     return render(request, 'reviews/whiskey_list.html', context)
 
 
+#  =====================================================
+# gets whiskey from DB given its ID and renders it using whiskey_detail.html.
+
 def whiskey_detail(request, whiskey_id):
+    whiskey = get_object_or_404(Whiskey, pk=whiskey_id)
+    return render(request, 'reviews/whiskey_detail.html', {'whiskey': whiskey, 'form': form})
+
+
+'''
+The add_review function is in charge of validating the form and creating the new review instance. The first thing it does is to use the request url whiskey ID to look for the whiskey we are going to add the review to. It will redirect the view to a 404 page if it doesn't find it. Otherwise, it will create a ReviewForm instance from the request POST data).
+'''
+
+
+def add_review(request, wine_id):
     whiskey = get_object_or_404(whiskey, pk=whiskey_id)
-    return render(request, 'reviews/whiskey_detail.html', {'whiskey': whiskey})
+    form = ReviewForm(request.POST)
+    if form.is_valid():
+        rating = form.cleaned_data['rating']
+        comment = form.cleaned_data['comment']
+        user_name = form.cleaned_data['user_name']
+        review = Review()
+        review.whiskey = whiskey
+        review.user_name = user_name
+        review.rating = rating
+        review.comment = comment
+        review.pub_date = datetime.datetime.now()
+        review.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('reviews:whiskey_detail', args=(whiskey.id,)))
+
+    return render(request, 'reviews/wine_detail.html', {'wine': wine, 'form': form})
